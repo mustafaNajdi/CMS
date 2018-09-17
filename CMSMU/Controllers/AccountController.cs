9ec347af8cc58e59a48f027b3f49eb0443ffe544
mustafaNajdi <mustafaneena@gmail.com>
2018-09-17 16:19:53 +0300

    add Db

---
+++ CMSMU/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMSMU.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            return RedirectToAction("Login");        
+        }
+
+    }
+}
