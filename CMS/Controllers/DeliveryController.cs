5db608aba52df65dcc129d1d8576ec7dcf56a725
mustafaNajdi <mustafaneena@gmail.com>
2018-09-19 22:58:19 +0300

    Added Views

--- CMS/Controllers/DeliveryController.cs
+++ CMS/Controllers/DeliveryController.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CMS.Models;
 
 namespace CMS.Controllers
 {
     public class DeliveryController : Controller
     {
+        CMSdb Context = new CMSdb();
         // GET: Delivery
         public ActionResult Index()
         {
-            return View();
+            List<DeliveryType> Del = new List<DeliveryType>();
+
+            Del = Context.DeliveryTypes.ToList();
+            return View(Del);
         }
         public ActionResult Create()
         {
+        
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(DeliveryType Del)
+        {
+            Context.DeliveryTypes.Add(Del);
+            Context.SaveChanges();
+            return RedirectToAction("index");
         }
         public ActionResult Update()
         {
--- CMS/Controllers/FacultyController.cs
+++ CMS/Controllers/FacultyController.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CMS.Models;
 
 namespace CMS.Controllers
 {
     public class FacultyController : Controller
     {
+        CMSdb Context = new CMSdb();
         // GET: Faculty
         public ActionResult Index()
         {
-            return View();
+            List<Faculty> Fcl = new List<Faculty>();
+
+            Fcl = Context.Faculties.ToList();
+            return View(Fcl);
         }
-        public ActionResult BatcIndex()
+        public ActionResult BatchIndex()
         {
             return View();
         }
@@ -21,13 +26,35 @@ namespace CMS.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult CreateFaculty(Faculty Fcl)
+        {
+            Context.Faculties.Add(Fcl);
+            Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
         public ActionResult CreateBatch()
         {
             return View();
         }
-        public ActionResult EditFaculty()
+        [HttpPost]
+        public ActionResult CreateBatch(Batch Bt)
         {
-            return View();
+            Context.Batches.Add(Bt);
+            Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult EditFaculty(int Id)
+        {
+            Faculty Fcl = Context.Faculties.Find(Id);
+            return View(Fcl);
+        }
+        [HttpPost]
+        public ActionResult EditFaculty(Faculty Fcl)
+        {
+            Context.Entry(Fcl).State = System.Data.Entity.EntityState.Modified;
+            Context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
