6b0429b9f0493794fa6d089450ad2f92d8e5c613
mustafaNajdi <mustafaneena@gmail.com>
2018-09-19 17:12:01 +0300

    Student Viw

--- CMS/Controllers/StudentController.cs
+++ CMS/Controllers/StudentController.cs
@@ -3,23 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CMS.Models;
 
 namespace CMS.Controllers
 {
     public class StudentController : Controller
     {
+        CMSdb Context = new CMSdb();
         // GET: Student
         public ActionResult Index()
         {
-            return View();
+            List<Student> students = new List<Student>();
+            students = Context.Students.ToList();
+            return View(students);
         }
         public ActionResult Create()
         {
             return View();
         }
-        public ActionResult Edit()
+        [HttpPost]
+        public ActionResult Create(Student stu)
         {
-            return View();
+            Context.Students.Add(stu);
+            Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int Id)
+        {
+            Student stu = Context.Students.Find(Id);
+            return View(stu);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student stu)
+        {
+            Context.Entry(stu).State = System.Data.Entity.EntityState.Modified;
+            Context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
