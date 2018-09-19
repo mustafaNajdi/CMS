060ff68f6f5fa199355868acc18659bef683e634
mustafaNajdi <mustafaneena@gmail.com>
2018-09-19 19:16:59 +0300

    Certificate Views

--- CMS/Controllers/CertificateController.cs
+++ CMS/Controllers/CertificateController.cs
@@ -3,23 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CMS.Models;
 
 namespace CMS.Controllers
 {
     public class CertificateController : Controller
     {
+        CMSdb Context = new CMSdb();
         // GET: Certificate
         public ActionResult Index()
         {
-            return View();
+            List<CertificateType> Cer = new List<CertificateType>();
+
+            Cer = Context.CertificateTypes.ToList();
+
+            return View(Cer);
         }
         public ActionResult Create()
         {
             return View();
         }
-        public ActionResult Edit()
+        [HttpPost]
+        public ActionResult Create(CertificateType Cer)
         {
-            return View();
+            Context.CertificateTypes.Add(Cer);
+            Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int Id)
+        {
+            
+            CertificateType Cer = Context.CertificateTypes.Find(Id);
+            return View(Cer);
+        }
+        [HttpPost]
+        public ActionResult Edit(CertificateType cer)
+        {
+            
+            Context.Entry(cer).State = System.Data.Entity.EntityState.Modified;
+            Context.SaveChanges();
+            return RedirectToAction("Index");
         }
         public ActionResult Delete()
         {
